b46b3d30dcb28842f8845254f3c84a69bbb4dbaa
NiponChi <nipon.chi@gmail.com>
2017-01-07 21:46:40 +0700

    Make Dockerfile without CMD validation pass - https://github.com/Runnable/validate-dockerfile/issues/14 - https://github.com/Runnable/validate-dockerfile/issues/19

--- src/main/utils/dockerfileUtil.js
+++ src/main/utils/dockerfileUtil.js
@@ -1,11 +1,14 @@
 import validateDockerfile from 'validate-dockerfile'
 import fsp from 'fs-promise'
 
+// Make Dockerfile without CMD validation pass
+var validateOpt = {quiet: true}
+
 function isDockerFile(filePath) {
   // TODO: check file size before load (fsp.stat())
   return fsp.readFile(filePath, {encoding:'utf8'})
     .then(contents => {
-      var ret = validateDockerfile(contents)
+      var ret = validateDockerfile(contents, validateOpt)
       var isValid = ret && ret.valid
       if (!isValid) {
         console.error(ret)
